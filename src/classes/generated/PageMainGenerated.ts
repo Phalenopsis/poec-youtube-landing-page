45cd60e25cd4318bdc930a982ebc0b1a8e9439cb
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-06 22:22:36 +0200

    feat: add class for main creation

---
+++ src/classes/generated/PageMainGenerated.ts
@@ -0,0 +1,11 @@
+import { HeaderGenerated } from "./HeaderGenerated.js";
+import { DivGenerator } from "../generator/DivGenerator.js";
+import { InputGenerator } from "../generator/InputGenerator.js";
+import { ButtonGenerator } from "../generator/ButtonGenerator.js";
+
+export class PageMainGenerated {
+    node: HTMLElement;
+    constructor(node: HTMLElement) {
+        this.node = node;
+    }
+}
