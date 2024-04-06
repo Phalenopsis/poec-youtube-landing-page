901cab34e7d3a982798d639a7d9a0da7bf0a5505
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-06 22:56:56 +0200

    feat: generate aside for main

--- src/classes/generated/PageMainGenerated.ts
+++ src/classes/generated/PageMainGenerated.ts
@@ -2,10 +2,17 @@ import { HeaderGenerated } from "./HeaderGenerated.js";
 import { DivGenerator } from "../generator/DivGenerator.js";
 import { InputGenerator } from "../generator/InputGenerator.js";
 import { ButtonGenerator } from "../generator/ButtonGenerator.js";
+import { AsideGenerator } from "../generator/AsideGenerator.js";
 
 export class PageMainGenerated {
     node: HTMLElement;
     constructor(node: HTMLElement) {
         this.node = node;
+        this.addAside();
+    }
+
+    addAside() {
+        const aside = AsideGenerator.generate();
+        this.node.appendChild(aside);
     }
 }
