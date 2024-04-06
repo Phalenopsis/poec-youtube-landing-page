5fae61be4f104cf1487f6c2aad32313b7e52647a
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-06 15:11:32 +0200

    feat: add a general generator for HTLMElement

---
+++ src/classes/generator/HTMLElementGenerator.ts
@@ -0,0 +1,21 @@
+export class HTMLElementGenerator {
+    static generateElement(
+        classNames: string[] | null = null,
+        id: string | null = null,
+        content: string | null = null,
+        element: string): HTMLElement {
+        const div = document.createElement(element);
+        if (!(null == classNames)) {
+            classNames.forEach(className => {
+                div.classList.add(className);
+            });
+        }
+        if (!(null == id)) {
+            div.id = id;
+        }
+        if (!(null == content)) {
+            div.innerText = content;
+        }
+        return div;
+    }
+}
