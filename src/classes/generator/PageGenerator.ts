b22816533e2f7576fd5c88f28d3a8a7f2957a396
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-06 22:23:26 +0200

    refactor: refactor with PageMaingenerated

--- src/classes/generator/PageGenerator.ts
+++ src/classes/generator/PageGenerator.ts
@@ -1,6 +1,7 @@
-import { DivGenerator } from "./DivGenerator.js";
 import { HeaderGenerator } from "./HeaderGenerator.js";
 import { PageHeaderGenerated } from "./../generated/PageHeaderGenerated.js";
+import { PageMainGenerated } from "./../generated/PageMainGenerated.js";
+import { MainGenerator } from "./MainGenerator.js";
 
 export class PageGenerator {
     static displayPage(): void {
@@ -10,16 +11,15 @@ export class PageGenerator {
 
     static displayPageHeader(): void {
         const header = new PageHeaderGenerated(
-            HeaderGenerator.generate(["page-header"], "page-header"));
+            HeaderGenerator.generate(["page-header"], "page-header")
+        );
         document.body.appendChild(header.node);
     }
 
     static displayPageMain(): void {
-        const main = document.createElement("main");
-        main.classList.add("page-main");
-        main.id = "page-main";
-        document.body.appendChild(main);
-        const test = DivGenerator.generate(["green", "big"], "greenId", "le petit chat vert");
-        main.appendChild(test);
+        const main = new PageMainGenerated(
+            MainGenerator.generate(["page-main"], "page-main")
+        );
+        document.body.appendChild(main.node);
     }
 }
