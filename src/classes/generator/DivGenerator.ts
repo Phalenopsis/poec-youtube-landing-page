fc34b47936f99251c61b517930917e32f5abbe82
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-06 15:12:20 +0200

    refactor: use HTMLElementGenerator

--- src/classes/generator/DivGenerator.ts
+++ src/classes/generator/DivGenerator.ts
@@ -1,17 +1,10 @@
+import { HTMLElementGenerator } from "./HTMLElementGenerator.js";
+
 export class DivGenerator {
-    static generateDiv(classNames: string[] | null = null, id: string | null = null, content: string | null = null): HTMLElement {
-        const div = document.createElement('div');
-        if (!(null == classNames)) {
-            classNames.forEach(className => {
-                div.classList.add(className);
-            });
-        }
-        if (!(null == id)) {
-            div.id = id;
-        }
-        if (!(null == content)) {
-            div.innerText = content;
-        }
-        return div;
+    static generateDiv(
+        classNames: string[] | null = null,
+        id: string | null = null,
+        content: string | null = null): HTMLElement {
+        return HTMLElementGenerator.generateElement(classNames, id, content, "div");
     }
 }
