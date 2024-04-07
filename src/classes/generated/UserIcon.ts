524845daf7fd18202f087274e0ea71f76282e4ae
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-07 18:43:12 +0200

    fix: use userName's 1st letter as symbol simplify userIcon in getUserIcon method

--- src/classes/generated/UserIcon.ts
+++ src/classes/generated/UserIcon.ts
@@ -4,13 +4,13 @@ export class UserIcon {
     private _node: HTMLElement;
     public constructor(content: string, color: string, backgroundColor: string) {
         this._node = this.getUserIcon();
-        this._node.innerText = content;
+        this._node.innerText = content[0];
         this._node.style.color = color;
         this._node.style.backgroundColor = backgroundColor;
     }
 
     private getUserIcon(): HTMLElement {
-        const userIcon = DivGenerator.generate(["user-icon", "centered"], null, "P");
+        const userIcon = DivGenerator.generate(["user-icon", "centered"]);
         return userIcon;
     }
     public get node(): HTMLElement {
